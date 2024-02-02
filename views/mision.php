c0858b14090f495edcaef9bc68b26c1d2e4d9551
gerar2024 <157903100+gerar2024@users.noreply.github.com>
2024-02-02 20:18:26 +0000

    eeeeeeeeeeeeeeee

---
+++ views/mision.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GYMBroo!</title>
+</head>
+<body>
+    <h1>MISION</h1>
+
+    <a href="/auth/login">Login</a>
+    <a href="/r">Registrarme</a>
+    <br><hr><br>
+    <a href="/mision">Misión</a>
+    <a href="/vision">Visión</a>
+    <a href="/filosofia">Filosofía</a>
+
+</body>
+</html>
